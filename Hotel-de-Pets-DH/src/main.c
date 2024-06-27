a251f7e6aaedbcc8f4b1d17c0c4da82e5faab8c3
helenards <140926468+helenards@users.noreply.github.com>
2024-06-27 00:46:25 -0300

    Create main.c

---
+++ Hotel-de-Pets-DH/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "ui.h"
+
+void show_main_menu() {
+  printf("Hotel de Pets DH\n");
+  printf("Cadastro do Tutor\n");
+  printf("Cadastro do Pet\n");
+  printf("Fazer reserva\n");
+  printf("Sair\n");
+}
+// seções da coluna do menu para selecionar
+
+int main() {
+  int choice;
+  do {
+    show_main_menu();
+    printf("Escolher uma opção: ");
+    scanf("%d", &choice); // chama a função selecionada
+  } while (choice != 4);
+
+  printf("Obrigada pela preferência! Volte sempre.\n");
+  printf("Att. Equipe Hotel de Pets DH");
+  return 0;
+}
