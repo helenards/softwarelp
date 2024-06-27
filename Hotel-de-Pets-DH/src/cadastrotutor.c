783111e8b6d9a5c662b06351fe46021166311e6c
helenards <140926468+helenards@users.noreply.github.com>
2024-06-27 01:29:36 -0300

    Add files via upload

---
+++ Hotel-de-Pets-DH/src/cadastrotutor.c
@@ -0,0 +1,55 @@
+#include "tutor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  int id;
+  char nome[50];
+  char CPF[11];
+  char endereco[50];
+  char telefone[15];
+  char email[50];
+  char senha[20];
+} Tutor;
+
+typedef enum { GATO, CACHORRO } EspeciePet;
+
+typedef struct {
+  int id;
+  char nome[50];
+  char raca[20];
+  char idade[2];
+  int tutor_id;
+} Pet;
+
+void cadastrar_tutor() {
+  Tutor novo_tutor;
+  FILE *file = fopen("donos.dat", "ab");
+  if (file == NULL) {
+    perror("Erro! Arquivo não pôde ser aberto.");
+    exit(1);
+  }
+
+  printf("Nome do tutor: \n");
+  scanf("%s", novo_tutor.nome);
+  printf("Telefone do tutor: \n");
+  scanf("%s", novo_tutor.telefone);
+  fwrite(&novo_tutor, sizeof(Tutor), 1, file);
+  fclose(file);
+  printf("Seu cadastro foi realizado com sucesso!\n");
+}
+
+void listar_tutores() {
+  Tutor tutor;
+  FILE *file = fopen("data/tutores.dat", "rb");
+  if (file == NULL) {
+    perror("Erro! Arquivo não pôde ser aberto.");
+    exit(1);
+  }
+  while (fread(&tutor, sizeof(Dono), 1, file)) {
+    printf("ID: %d, Nome: %s, Telefone: %s\n", tutor.id, tutor.nome,
+           tutor.telefone);
+  }
+  fclose(file);
+}
